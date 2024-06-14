edfdfbb7f75d231787f328309a1cd29c959c1b14
Olajumoke Sekoni <olajumoke.sekoni@cyber.cyberspace.net.ng>
2024-06-14 15:05:37 +0100

    wip

---
+++ src/screens/setup/faculty/data.ts
@@ -0,0 +1,16 @@
+export const data = [
+    { key: 1, level: "Foundation", status: "Passed" },
+    { key: 2, level: "Intermediate I", status: "Failed" },
+    { key: 3, level: "Intermediate II", status: "2 references" },
+    {
+      key: 4,
+      level: "Professional Examination I",
+      status: "Withheld",
+    },
+    {
+      key: 5,
+      level: "Professional Examination II",
+      status: "Pending",
+    },
+  ];
+  
--- src/screens/setup/faculty/faculty.tsx
+++ src/screens/setup/faculty/faculty.tsx
@@ -1,17 +1,76 @@
-import PageLayout from "../../../layouts/pageLayout/pageLayout"
+import PageLayout from "../../../layouts/pageLayout/pageLayout";
 import { ReactComponent as GraterThan } from "../../../assets/chevron_forward.svg";
+import { ReactComponent as Action } from "../../../assets/more_horiz.svg";
+import { ReactComponent as Add } from "../../../assets/add.svg";
+import { Table } from "antd";
+import { data } from "./data";
+import styles from "./styles.module.scss"
+import Button from "../../../custom/button/button";
 
-
-const FaultySetUp = () =>{
-    return(
-        <PageLayout
+const FaultySetUp = () => {
+  const column = [
+    {
+      title: "Email",
+      dataIndex: "Email",
+      key: "Email",
+    },
+    {
+      title: "Phone No.",
+      dataIndex: "Phone",
+      key: "Phone",
+      render: (text: string) => <span>{text || "N/A"}</span>,
+    },
+    {
+      title: "Student Name",
+      dataIndex: "StudenName",
+      key: "StudenName",
+      render: (text: string) => <span>{text || "N/A"}</span>,
+    },
+    {
+      title: "Date",
+      dataIndex: "TransDate",
+      key: "TransDate",
+    },
+    {
+      title: "Action",
+      dataIndex: "TransDate",
+      key: "TransDate",
+      render: (text: string) => (
+        <span>
+          {
+            <>
+              {" "}
+              <Action />{" "}
+            </>
+          }
+        </span>
+      ),
+    },
+  ];
+  return (
+    <main>
+      <PageLayout
         paragraph="Faculty Setup"
-        firstText="Setup"
+        firstText="Setup Programs"
         secondText="Faculty Setup"
-        iconBefore={<GraterThan/>}
-        />
-    )
+        iconBefore={<GraterThan />}
+        headerActions={
+            <Button iconBefore={<Add />} text="Setup" />
 
-}
+        }
+      />
 
-export default FaultySetUp
+      <Table
+        columns={column}
+        dataSource={data}
+        className={styles.row}
+        rowKey={"DueYear"}
+        scroll={{ x: 400 }}
+        //   pagination={{ current: currentPage, pageSize: pageSize, onChange: handlePaginationChange, position: ["bottomCenter"] }}
+        pagination={false}
+      />
+    </main>
+  );
+};
+
+export default FaultySetUp;
